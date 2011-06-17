4360b47d4f46edfd6c5662931e8ad24b524f3fd7
mediamadness <daniel@mediamadness.de>
2011-06-17 09:50:32 +0000

    fixed typo

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <?php
     
     // set include path in php.ini to current paths + the one in which the framework resides (temporary)
-    ini_set( 'include_path', ini_get('include_path') . PATH_SEPERATOR . '../library/' );
+    ini_set( 'include_path', ini_get('include_path') . PATH_SEPARATOR . '../library/' );
     
     // Classloader
     include_once('Onyx/Loader.php');
